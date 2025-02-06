a80cd4d3da26702381f552d075c50dca550f28e6
retsen <jandenbronk@mail.ru>
2025-02-06 19:52:48 +0900

    Work with interface

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColor.layer.cornerRadius = 75
+        yellowColor.layer.cornerRadius = 75
+        greenColor.layer.cornerRadius = 75
+        
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
     }
-
-
+    
+    @IBAction func startButton(_ sender: Any) {
+        redColor.alpha = 1
+    }
 }
-
